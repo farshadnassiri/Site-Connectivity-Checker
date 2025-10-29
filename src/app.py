a81476fa9069bd636d371e7732e99f583ef3e9ab
Farshad Nassiri <farshadnassiri@gmail.com>
2025-10-29 12:29:39 +0330

    check_site_availability

--- src/app.py
+++ src/app.py
@@ -12,8 +12,19 @@ def foramt_url(url):
         url=url[len("www."):]
     formatted_url="https://www." +url    
     return formatted_url
-    
-        
+
+def check_site_availability(url):
+    try:
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/58.0.3029.110 Safari/537.36'
+        }    
+        response=requests.get(url,timeout=5,headers=headers)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+            
 
 
 
