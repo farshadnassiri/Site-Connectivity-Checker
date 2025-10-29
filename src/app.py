9aa8bc4245c252c9e26a77a8ed177c87a00cd17b
Farshad Nassiri <farshadnassiri@gmail.com>
2025-10-29 11:50:43 +0330

    format url func

--- src/app.py
+++ src/app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import requests
+import concurrent.futures
+
+
+def foramt_url(url):
+    if url.startswith("http://"):
+        url=url[len("http://"):]
+    elif url.startswith('https://'):
+        url = url[len('https://'):]
+    if url.startswith("www."):
+        url=url[len("www."):]
+    formatted_url="https://www." +url    
+    return formatted_url
+    
+        
+
 
 
